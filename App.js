162c71316a290a98096be2d0da7353db75ddc18b
Janeth1513 <94811417+Janeth1513@users.noreply.github.com>
2022-06-07 23:07:00 -0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, Text} from 'react-native';
+
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try{
+    const response = await fetch('https://reactnative.dev/movies.json'); //tarea 1
+    const json = await response.json(); //tarea 2'
+    setData(json.movies);
+
+  } catch (error){
+    console.error(error);
+  }
+}
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}</Text>
+          )}
+        />
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
